ad093283bdf6f610fba8ab7af18e311c8edc7427
simon mekit <simonmekit@gmail.com>
2023-12-21 23:08:37 -0500

    git restore command

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello!");
         System.out.println("Second update");
         System.out.println("Third update!");
+        System.out.println("check git restore command - discard changes to a file in working directory!");
     }
 }
